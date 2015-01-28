36f621a72c35f1fab268c486bf0529422868e9d9
Félix Pelletier <felixpelletier23@gmail.com>
2015-01-28 16:38:03 -0500

    Testing shared_ptr

--- Python/src/module.cpp
+++ Python/src/module.cpp
@@ -3,12 +3,42 @@
 #include <Pose.h>
 #include <Position.h>
 #include <boost/python.hpp>
+#include <iostream>
+
+std::shared_ptr<Rule::RobotCommand> statement;
 
 void sendCommand(Rule::RobotCommand robotCommand);
 
 void sendCommand(Rule::RobotCommand robotCommand){
             std::shared_ptr<Rule::RobotCommand> robotCommand_p = std::make_shared<Rule::RobotCommand>(robotCommand);
             Rule::GameRoot::getSingleton().addRobotCommand(robotCommand_p);
+}
+
+std::shared_ptr<Rule::RobotCommand> getFizzy(){
+
+	std::shared_ptr<Rule::RobotCommand> fizzy = std::make_shared<Rule::RobotCommand>();
+
+	fizzy->isTeamYellow = true;
+	fizzy->dribble = true;
+	fizzy->dribbleSpeed = 1;
+	fizzy->kick = true;
+	fizzy->kickSpeed = 2;
+	fizzy->robotId = 1;
+	fizzy->stop = false;
+	fizzy->pose.coord.x = 342;
+	fizzy->pose.coord.y = rand() % 2;
+	fizzy->pose.orientation = 5;
+
+	std::cout << "lol\n";
+
+	statement = fizzy;
+
+	return fizzy;
+}
+
+std::shared_ptr<Rule::RobotCommand> getStatement(){
+
+	return statement;
 }
 
 //Creates the POC module in python containing various utilities (PLUGINS)
@@ -18,6 +48,19 @@ BOOST_PYTHON_MODULE(rule_python)
     using namespace Rule;
 
     def("sendCommand", &sendCommand);
+    def("getFizzy", &getFizzy);
+    def("getStatement", &getStatement);
+
+    class_<RobotCommand, std::shared_ptr<RobotCommand> >("RobotCommand_p",init<>())
+            .def_readwrite("isTeamYellow", &RobotCommand::isTeamYellow)
+	    .def_readwrite("dribble", &RobotCommand::dribble)
+	    .def_readwrite("dribbleSpeed", &RobotCommand::dribbleSpeed)
+	    .def_readwrite("kick", &RobotCommand::kick)
+	    .def_readwrite("kickSpeed", &RobotCommand::kickSpeed)
+	    .def_readwrite("robotId", &RobotCommand::robotId)
+	    .def_readwrite("stop", &RobotCommand::stop)
+	    .def_readwrite("pose", &RobotCommand::pose);
+	    ;
 
     class_<RobotCommand>("RobotCommand")
 	    .def_readwrite("isTeamYellow", &RobotCommand::isTeamYellow)
--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,12 @@ import rule_python as rule
 
 def update():
 
+    fizzy = rule.getFizzy()
+    print(fizzy.pose.coord.x)
+    statement = rule.getStatement()
+    print(statement.pose.coord.x)
+    lol = statement
+    print(lol.pose.coord.x)
     for i in range(2):
         for j in range(5):
             rc = rule.RobotCommand()
