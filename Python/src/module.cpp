22647362a9a9e08ec0034790ec99fe3cc6eb361b
Félix Pelletier <felixpelletier23@gmail.com>
2015-02-03 18:45:01 -0500

    Cleanup of python module

--- Python/src/module.cpp
+++ Python/src/module.cpp
@@ -5,39 +5,10 @@
 #include <boost/python.hpp>
 #include <iostream>
 
-std::shared_ptr<Rule::RobotCommand> statement;
-
 void sendCommand(std::shared_ptr<Rule::RobotCommand> robotCommand);
 
 void sendCommand(std::shared_ptr<Rule::RobotCommand> robotCommand){
-            //std::shared_ptr<Rule::RobotCommand> robotCommand_p = std::make_shared<Rule::RobotCommand>(robotCommand);
-            //Rule::GameRoot::getSingleton().addRobotCommand(robotCommand_p);
             Rule::GameRoot::getSingleton().addRobotCommand(robotCommand);
-}
-
-std::shared_ptr<Rule::RobotCommand> getFizzy(){
-
-	std::shared_ptr<Rule::RobotCommand> fizzy = std::make_shared<Rule::RobotCommand>();
-
-	fizzy->isTeamYellow = true;
-	fizzy->dribble = true;
-	fizzy->dribbleSpeed = 1;
-	fizzy->kick = true;
-	fizzy->kickSpeed = 2;
-	fizzy->robotId = 1;
-	fizzy->stop = false;
-	fizzy->pose.coord.x = 342;
-	fizzy->pose.coord.y = rand() % 2;
-	fizzy->pose.orientation = 5;
-
-	statement = fizzy;
-
-	return fizzy;
-}
-
-std::shared_ptr<Rule::RobotCommand> getStatement(){
-
-	return statement;
 }
 
 //Creates the POC module in python containing various utilities (PLUGINS)
@@ -66,6 +37,8 @@ BOOST_PYTHON_MODULE(rule_python)
 	    .def_readwrite("x", &Position::x)
 	    .def_readwrite("y", &Position::y)
 	    .def_readwrite("z", &Position::z);
+
+
 
 }
 
