8771bd094e3fe2fedaefcc10637718aad5bc6df7
Félix Pelletier <felixpelletier23@gmail.com>
2015-02-10 13:21:21 -0500

    Some commentaries on python module

--- Python/src/module.cpp
+++ Python/src/module.cpp
@@ -22,7 +22,7 @@ Rule::PythonVisionFrame getFrame(){
 	return pframe;
 }
 
-//Creates the POC module in python containing various utilities (PLUGINS)
+//Creates the rule_python module in python containing various utilities (PLUGINS)
 BOOST_PYTHON_MODULE(rule_python)
 {
     using namespace boost::python;
@@ -31,6 +31,7 @@ BOOST_PYTHON_MODULE(rule_python)
     def("sendCommand", &sendCommand);
     def("getFrame", &getFrame);
 
+    //RobotCommand
     class_<RobotCommand, std::shared_ptr<RobotCommand>>("RobotCommand", init<>())
             .def_readwrite("isTeamYellow", &RobotCommand::isTeamYellow)
 	    .def_readwrite("dribble", &RobotCommand::dribble)
@@ -40,16 +41,18 @@ BOOST_PYTHON_MODULE(rule_python)
 	    .def_readwrite("robotId", &RobotCommand::robotId)
 	    .def_readwrite("stop", &RobotCommand::stop)
 	    .def_readwrite("pose", &RobotCommand::pose);
-    
+   
+    //Pose
     class_<Pose, std::shared_ptr<Pose>>("Pose")
 	    .def_readwrite("coord", &Pose::coord)
 	    .def_readwrite("orientation", &Pose::orientation);
-
+    //Position
     class_<Position, std::shared_ptr<Position>>("Position")
 	    .def_readwrite("x", &Position::x)
 	    .def_readwrite("y", &Position::y)
 	    .def_readwrite("z", &Position::z);
 
+    //VisionFrame
     class_<PythonVisionFrame>("VisionFrame", no_init)
 	    .def_readonly("cameraId", &PythonVisionFrame::cameraId)
 	    .def_readonly("frameId", &PythonVisionFrame::frameId)
@@ -57,10 +60,10 @@ BOOST_PYTHON_MODULE(rule_python)
 	    .def_readonly("balls", &PythonVisionFrame::balls)
 	    .def_readonly("teams", &PythonVisionFrame::teams);
     
-    class_<VisionFrame::Ball>("Ball", no_init)
+    class_<VisionFrame::Ball>("Ball", no_init) //Should this be in it's own namespace?
     	    .def_readonly("position", &VisionFrame::Ball::position);
 
-    class_<VisionFrame::Team::Robot>("Robot", no_init)
+    class_<VisionFrame::Team::Robot>("Robot", no_init) //Should this be in it's own namespace?
 	    .def_readonly("robotId", &VisionFrame::Team::Robot::robotId) 
 	    .def_readonly("pose", &VisionFrame::Team::Robot::pose);
 
@@ -68,15 +71,17 @@ BOOST_PYTHON_MODULE(rule_python)
             .def_readonly("teamId", &PythonVisionFrame::Team::teamId)
             .def_readonly("robots", &PythonVisionFrame::Team::robots)
             .def_readonly("robotCount", &PythonVisionFrame::Team::robotCount);
+
+    //RefereeCommand
     
-    class_<PythonRefereeCommand>("PythonRefereeCommand", no_init)
+    class_<PythonRefereeCommand>("RefereeCommand", no_init)
 	    .def_readonly("command" , &PythonRefereeCommand::command)
 	    .def_readonly("packetTimeStamp" , &PythonRefereeCommand::packetTimeStamp)
 	    .def_readonly("stage" , &PythonRefereeCommand::stage)
 	    .def_readonly("teams" , &PythonRefereeCommand::teams)
 	    .def_readonly("teamCount" , &PythonRefereeCommand::teamCount);
     
-    class_<PythonRefereeCommand::Team>("PythonRefereeCommand.Team", no_init)
+    class_<PythonRefereeCommand::Team>("RefereeCommand.Team", no_init)
 	    .def_readonly("goalieCount" , &PythonRefereeCommand::Team::goalieCount)
 	    .def_readonly("name" , &PythonRefereeCommand::Team::name)
 	    .def_readonly("redCardsCount" , &PythonRefereeCommand::Team::redCardsCount)
@@ -86,11 +91,11 @@ BOOST_PYTHON_MODULE(rule_python)
 	    .def_readonly("yellowCardsRemainingTime" , &PythonRefereeCommand::Team::yellowCardsRemainingTime)
 	    .def_readonly("yellowCardsRemainingTimeCount" , &PythonRefereeCommand::Team::yellowCardsRemainingTimeCount);
 
-    class_<RefereeCommand::Stage>("Stage", no_init)
+    class_<RefereeCommand::Stage>("Stage", no_init) //Should this be in it's own namespace?
 	    .def_readonly("name" , &RefereeCommand::Stage::name)
 	    .def_readonly("timeLeft" , &RefereeCommand::Stage::timeLeft);
 
-    class_<RefereeCommand::Command>("Command", no_init)
+    class_<RefereeCommand::Command>("Command", no_init) //Should this be in it's own namespace?
 	    .def_readonly("name" , &RefereeCommand::Command::name)
 	    .def_readonly("timeStamp" , &RefereeCommand::Command::timeStamp);
 
