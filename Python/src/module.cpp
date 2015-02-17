bfecfc12a778a841be6c57ed2003d4f0feb24ae4
Félix Pelletier <felixpelletier23@gmail.com>
2015-02-16 23:21:42 -0500

    Added documentation for RobotCommand

--- Python/src/module.cpp
+++ Python/src/module.cpp
@@ -22,7 +22,7 @@ BOOST_PYTHON_MODULE(rule_python)
     def("send_command", &sendCommand, "Send a robot_command to the engine");
 
     //RobotCommand
-    class_<RobotCommand, std::shared_ptr<RobotCommand>>("robot_command", init<>())
+    class_<RobotCommand, std::shared_ptr<RobotCommand>>("RobotCommand", "This is a command sent to a specific robot in a specific team. It is created by you, the coach.", init<>())
             .def_readwrite("is_team_yellow", &RobotCommand::isTeamYellow)
 	    .def_readwrite("dribble", &RobotCommand::dribble)
 	    .def_readwrite("dribble_speed", &RobotCommand::dribbleSpeed)
@@ -33,12 +33,12 @@ BOOST_PYTHON_MODULE(rule_python)
 	    .def_readwrite("pose", &RobotCommand::pose);
    
     //Pose
-    class_<Pose, std::shared_ptr<Pose>>("pose")
-	    .def_readwrite("coord", &Pose::coord, "C'est la coordonée, dumbass")
-	    .def_readwrite("orientation", &Pose::orientation, "C'est l'orientation, dumbass");
+    class_<Pose, std::shared_ptr<Pose>>("Pose", "This contains the robot's coordinates and orientation")
+	    .def_readwrite("coord", &Pose::coord)
+	    .def_readwrite("orientation", &Pose::orientation);
 
     //Position
-    class_<Position, std::shared_ptr<Position>>("position")
+    class_<Position, std::shared_ptr<Position>>("Position", "If you don't know what this is...")
 	    .def_readwrite("x", &Position::x)
 	    .def_readwrite("y", &Position::y)
 	    .def_readwrite("z", &Position::z);
@@ -46,20 +46,20 @@ BOOST_PYTHON_MODULE(rule_python)
     //VisionFrame
     {
     scope vision_frame =
-    class_<PythonVisionFrame>("vision_frame", no_init)
+    class_<PythonVisionFrame>("VisionFrame", "This is a frame sent by the Vision system. It is the state of the game at a precise point in time", no_init)
 	    .def_readonly("camera_id", &PythonVisionFrame::cameraId)
 	    .def_readonly("frame_id", &PythonVisionFrame::frameId)
 	    .def_readonly("balls", &PythonVisionFrame::balls)
 	    .def_readonly("teams", &PythonVisionFrame::teams);
     
-    class_<VisionFrame::Ball>("ball", no_init) //Should this be in it's own namespace?
+    class_<VisionFrame::Ball>("Ball", no_init)
     	    .def_readonly("position", &VisionFrame::Ball::position);
 
-    class_<VisionFrame::Team::Robot>("robot", no_init) //Should this be in it's own namespace?
+    class_<VisionFrame::Team::Robot>("Robot", no_init)
 	    .def_readonly("robot_id", &VisionFrame::Team::Robot::robotId) 
 	    .def_readonly("pose", &VisionFrame::Team::Robot::pose);
 
-    class_<PythonVisionFrame::Team>("team", no_init)
+    class_<PythonVisionFrame::Team>("Team", no_init)
             .def_readonly("team_id", &PythonVisionFrame::Team::teamId)
             .def_readonly("robots", &PythonVisionFrame::Team::robots);
     }
@@ -67,13 +67,16 @@ BOOST_PYTHON_MODULE(rule_python)
     //RefereeCommand
     {
     scope referee_command =
-    class_<PythonRefereeCommand>("referee_command", no_init)
+    class_<PythonRefereeCommand>("RefereeCommand",
+		    		 "| This is a command sent by the referee (duh).\n"
+				 "| It information such as a command (an order from the referee), the list of penalties (yellow and red cards), and the time left in the game.\n"
+				 "| The referee system always sends these commands even if the state of the game has not changed.\n", no_init)
 	    .def_readonly("command" , &PythonRefereeCommand::command)
 	    .def_readonly("packet_time_stamp" , &PythonRefereeCommand::packetTimeStamp)
 	    .def_readonly("stage" , &PythonRefereeCommand::stage)
 	    .def_readonly("teams" , &PythonRefereeCommand::teams);
     
-    class_<PythonRefereeCommand::Team>("team", no_init)
+    class_<PythonRefereeCommand::Team>("Team", no_init)
 	    .def_readonly("goalie_count" , &PythonRefereeCommand::Team::goalieCount)
 	    .def_readonly("name" , &PythonRefereeCommand::Team::name)
 	    .def_readonly("red_cards_count" , &PythonRefereeCommand::Team::redCardsCount)
@@ -82,11 +85,11 @@ BOOST_PYTHON_MODULE(rule_python)
 	    .def_readonly("yellow_cards_count" , &PythonRefereeCommand::Team::yellowCardsCount)
 	    .def_readonly("yellow_cards_remaining_time" , &PythonRefereeCommand::Team::yellowCardsRemainingTime);
 
-    class_<RefereeCommand::Stage>("stage", no_init)
+    class_<RefereeCommand::Stage>("Stage", no_init)
 	    .def_readonly("name" , &RefereeCommand::Stage::name)
 	    .def_readonly("time_left" , &RefereeCommand::Stage::timeLeft);
 
-    class_<RefereeCommand::Command>("Command", no_init) //starts with uppercase to prevent name collision
+    class_<RefereeCommand::Command>("Command", no_init) 
 	    .def_readonly("name" , &RefereeCommand::Command::name)
 	    .def_readonly("time_stamp" , &RefereeCommand::Command::timeStamp);
     }
